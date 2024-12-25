8fa59e20147cbf8c50a45810b437118f1d0d5ae3
MohammedSoliman11 <mohammedsoliman404@gmail.com>
2024-12-25 19:35:11 +0200

    update about section

--- src/components/aboutUs.tsx
+++ src/components/aboutUs.tsx
@@ -1,12 +1,14 @@
 import about from "../assets/images/Rectangle 15.png";
+import vector from "../assets/images/Vector (3).png";
+import vector2 from "../assets/images/Vector (5).png";
 import "../styles/aboutUs.css";
 const AboutUs = () => {
   return (
     <>
       {/* Section 2 */}
       <div id='about-us' className='container section2 mb-5'>
-        <h5 className='mb-5 text-center mediumFont'>
-          عن <span className='underLine'>الشركة</span>
+        <h5 className='mb-5 text-center mediumFont brandMain-color'>
+          عن <span className='underLine secondary-color'>الشركة</span>
         </h5>
         <div className='row align-items-center'>
           {/* Right Section - Text */}
@@ -37,6 +39,41 @@ const AboutUs = () => {
                 className='img-fluid rounded'
               />
               <div className='dotted-box'></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Section 3 */}
+      <div className='section3 container-fluid d-flex justify-content-center align-items-center'>
+        <div className='container-fluid'>
+          <h5 className='text-center boldFont text-white'>
+            كلمة مدير <span className='secondary-color underLine'>المؤسسة</span>
+          </h5>
+          <div className='regularFont d-flex align-content-center justify-content-center mt-4'>
+            <div className='row'>
+              <div className='col-md-1 firstQuoteIcon d-flex align-items-start justify-content-end pt-4'>
+                <img src={vector} alt='' className='' />
+              </div>
+              <div className='col-md-9 quoteText'>
+                <p className='text-white'>
+                  إنــــه لمــن دواعــي فخـــــــــرنا واعتزازنا أن نكـــــون
+                  بهذا الصــــــرح العملاق، متميــــــــزون بجودة ما نقدمه
+                  لعملائنا وحرصاً منا على أن نكون على قدر الثقة التي نحظــــى
+                  بها في العديـــــــــد من الأعمال والمشاريع،نتطلع
+                  لخـــــدمتكـم على أكمــل وجــــه من خـــــلال فـــــريق
+                  عمـــــــــــــــل متخصص همـــه الأول ارضـــاؤكـــــم،
+                  والعمـــــل على إنجـــــــاز الأعمال وتنفيذها بأفضل
+                  المواصــفات وفقاً للأصول الفنية لأن تحقيــــق رضــــــا
+                  العمــلاء وإتمــــام الأعمــــــــــــــال الموكلــــة
+                  إلينــــا في الوقت المحــدد هي ركزتين أســــــاسيتين للتميز
+                  والتطـــــــوير والاستمـــــــــــــــرارية في ســــــــوق
+                  العمل بنجـــــــاح
+                </p>
+              </div>
+              <div className='col-md-1 secondQuoteIcon d-flex align-items-end justify-content-start pb-5'>
+                <img src={vector2} alt='' className='' />
+              </div>
             </div>
           </div>
         </div>
