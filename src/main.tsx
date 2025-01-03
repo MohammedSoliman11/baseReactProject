9233e55745d1d269f0aae79b5f3580f2ee96ece4
MohammedSoliman11 <mohammedsoliman404@gmail.com>
2025-01-03 17:36:36 +0200

    update routes

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,16 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { BrowserRouter } from "react-router-dom";
 // import { ThemeProvider } from "./ThemeProvider";
 // Import global styles if needed
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     {/* <ThemeProvider> */}
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
     {/* </ThemeProvider> */}
   </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  root: ".", // Set project root (where index.html is located)
   plugins: [react()],
-  build: {
-    outDir: "dist", // Output directory for production build
-    rollupOptions: {
-      input: "./index.html", // Path to index.html
-    },
-  },
 });
