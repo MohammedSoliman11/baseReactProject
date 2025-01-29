80a79057fd69b5d568563f9dbe8e26b18b675e23
jihad shehata <jihadgamal808@gmail.com>
2025-01-29 01:38:12 -0800

    apply mediaQuery

--- src/components/landing-page-components/forms.tsx
+++ src/components/landing-page-components/forms.tsx
@@ -2,6 +2,11 @@ import { useState } from "react";
 import "../../styles/landing-page-styles/forms.css";
 import KSAFlag from "../../assets/image.png";
 
+function getServiceOrderForm() {
+  // ServiceOrder Form Function for registration
+  console.log("getServiceOrderForm");
+}
+
 const Form = () => {
   // State to track the active form
   const [activeForm, setActiveForm] = useState("join"); // 'join' or 'service'
@@ -10,14 +15,16 @@ const Form = () => {
   const showJoinForm = () => setActiveForm("join");
   const showServiceForm = () => setActiveForm("service");
 
+  // ServiceOrder Form Function for registration
+
   return (
     <div
-      id='join'
-      className='shadow-lg formMain container d-flex justify-content-center'
+      id="join"
+      className="shadow-lg formMain container d-flex justify-content-center"
     >
       {/* Toggle Buttons */}
-      <div className='col-md-10'>
-        <div className='d-flex justify-content-center mb-4 tabs mediumFont'>
+      <div className="col-md-10">
+        <div className="d-flex justify-content-center mb-4 tabs mediumFont">
           <button
             className={`col-md-6 btn fontSize-32 ${
               activeForm === "join" ? "active" : ""
@@ -46,180 +53,180 @@ const Form = () => {
 
 // "طلب انضمام" Form
 const JoinForm = () => (
-  <form className='mediumFont'>
-    <div className='mb-3'>
-      <div className='input-container'>
-        <label className='input-label'>الاسم الثلاثي</label>
-        <span className='required'>*</span>
+  <form className="mediumFont">
+    <div className="mb-3">
+      <div className="input-container">
+        <label className="input-label">الاسم الثلاثي</label>
+        <span className="required">*</span>
         <input
-          type='text'
-          id='fullName'
-          placeholder='ادخل الاسم'
-          className='input-field'
+          type="text"
+          id="fullName"
+          placeholder="ادخل الاسم"
+          className="input-field"
         />
       </div>
     </div>
-    <div className='mb-3'>
-      <div className='input-container'>
-        <label className='input-label'>البريد الالكتروني</label>
-        <span className='required'>*</span>
+    <div className="mb-3">
+      <div className="input-container">
+        <label className="input-label">البريد الالكتروني</label>
+        <span className="required">*</span>
         <input
-          type='email'
-          id='fullName'
-          placeholder='ادخل البريد الالكتروني الخاص بك'
-          className='input-field'
+          type="email"
+          id="fullName"
+          placeholder="ادخل البريد الالكتروني الخاص بك"
+          className="input-field"
         />
       </div>
     </div>
-    <div className='mb-3'>
-      <div className='phone-input-container'>
-        <label className='input-label'>رقم الجوال</label>
-        <span className='required'>*</span>
-        <div className='phone-input-wrapper'>
-          <select className='country-code'>
-            <option value='+966'>+966</option>
-            <option value='+1'>+1</option>
-            <option value='+44'>+44</option>
-            <option value='+91'>+91</option>
+    <div className="mb-3">
+      <div className="phone-input-container">
+        <label className="input-label">رقم الجوال</label>
+        <span className="required">*</span>
+        <div className="phone-input-wrapper">
+          <select className="country-code">
+            <option value="+966">+966</option>
+            <option value="+1">+1</option>
+            <option value="+44">+44</option>
+            <option value="+91">+91</option>
           </select>
-          <div className='flag'>
-            <img src={KSAFlag} alt='Saudi Arabia' />
+          <div className="flag">
+            <img src={KSAFlag} alt="Saudi Arabia" />
           </div>
           <input
-            type='text'
-            id='mobile'
-            placeholder='رقم الهاتف'
-            className='phone-input'
+            type="text"
+            id="mobile"
+            placeholder="رقم الهاتف"
+            className="phone-input"
           />
         </div>
       </div>
     </div>
-    <div className='mb-3'>
-      <div className='input-container'>
-        <label className='input-label'>الجنس</label>
-        <select name='gender' id='gender' className='input-field'>
-          <option value='Male'>ذكر</option>
-          <option value='Femail'>انثي</option>
+    <div className="mb-3">
+      <div className="input-container">
+        <label className="input-label">الجنس</label>
+        <select name="gender" id="gender" className="input-field">
+          <option value="Male">ذكر</option>
+          <option value="Femail">انثي</option>
         </select>
       </div>
     </div>
-    <div className='mb-3'>
-      <div className='input-container'>
-        <label className='input-label'>اسم الشركة / المؤسسة</label>
-        <span className='required'>*</span>
+    <div className="mb-3">
+      <div className="input-container">
+        <label className="input-label">اسم الشركة / المؤسسة</label>
+        <span className="required">*</span>
         <input
-          type='text'
-          id='companyName'
-          placeholder='ادخل اسم الشركة'
-          className='input-field'
+          type="text"
+          id="companyName"
+          placeholder="ادخل اسم الشركة"
+          className="input-field"
         />
       </div>
     </div>
-    <div className='mb-3'>
-      <div className='input-container'>
-        <label className='input-label'>السجل التجاري</label>
-        <span className='required'>*</span>
+    <div className="mb-3">
+      <div className="input-container">
+        <label className="input-label">السجل التجاري</label>
+        <span className="required">*</span>
         <input
-          type='text'
-          id='tradeNumber'
-          placeholder='ادخل رقم السجل التجاري'
-          className='input-field'
+          type="text"
+          id="tradeNumber"
+          placeholder="ادخل رقم السجل التجاري"
+          className="input-field"
         />
       </div>
     </div>
-    <div className='mb-3'>
-      <div className='input-container'>
-        <label className='input-label'>السجل الضريبي</label>
-        <span className='required'>*</span>
+    <div className="mb-3">
+      <div className="input-container">
+        <label className="input-label">السجل الضريبي</label>
+        <span className="required">*</span>
         <input
-          type='text'
-          id='taxNumber'
-          placeholder='ادخل رقم السجل الضريبي'
-          className='input-field'
+          type="text"
+          id="taxNumber"
+          placeholder="ادخل رقم السجل الضريبي"
+          className="input-field"
         />
       </div>
     </div>
-    <div className='mb-3'>
-      <div className='input-container'>
-        <label className='input-label'>لماذا تريد الانضمام الينا ؟</label>
+    <div className="mb-3">
+      <div className="input-container">
+        <label className="input-label">لماذا تريد الانضمام الينا ؟</label>
         <textarea
-          placeholder='اكتب لماذا تريد الانضمام الينا ؟'
-          className='input-field'
+          placeholder="اكتب لماذا تريد الانضمام الينا ؟"
+          className="input-field"
           rows={5}
         />
       </div>
     </div>
-    <button className='gradient-button'>
-      <span className='button-text'>ارسال طلب انضمام</span>
-      <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+    <button className="gradient-button">
+      <span className="button-text">ارسال طلب انضمام</span>
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
     </button>
   </form>
 );
 
 // "طلب خدمة" Form
 const ServiceForm = () => (
-  <form className='mediumFont'>
-    <div className='mb-3'>
-      <div className='input-container'>
-        <label className='input-label'>الاسم الثلاثي</label>
-        <span className='required'>*</span>
+  <form className="mediumFont" onSubmit={getServiceOrderForm}>
+    <div className="mb-3">
+      <div className="input-container">
+        <label className="input-label">الاسم الثلاثي</label>
+        <span className="required">*</span>
         <input
-          type='text'
-          id='fullName'
-          placeholder='ادخل الاسم'
-          className='input-field'
+          type="text"
+          id="fullName"
+          placeholder="ادخل الاسم"
+          className="input-field"
         />
       </div>
     </div>
-    <div className='phone-input-container'>
-      <label className='input-label'>
-        <span className='required'>*</span> رقم الجوال
+    <div className="phone-input-container">
+      <label className="input-label">
+        <span className="required">*</span> رقم الجوال
       </label>
-      <div className='phone-input-wrapper'>
-        <select className='country-code'>
-          <option value='+966'>+966</option>
-          <option value='+1'>+1</option>
-          <option value='+44'>+44</option>
-          <option value='+91'>+91</option>
+      <div className="phone-input-wrapper">
+        <select className="country-code">
+          <option value="+966">+966</option>
+          <option value="+1">+1</option>
+          <option value="+44">+44</option>
+          <option value="+91">+91</option>
         </select>
-        <div className='flag'>
-          <img src={KSAFlag} alt='Saudi Arabia' />
+        <div className="flag">
+          <img src={KSAFlag} alt="Saudi Arabia" />
         </div>
         <input
-          type='text'
-          id='mobile'
-          placeholder='رقم الهاتف'
-          className='phone-input'
+          type="text"
+          id="mobile"
+          placeholder="رقم الهاتف"
+          className="phone-input"
         />
       </div>
     </div>
-    <div className='mb-3'>
-      <div className='input-container'>
-        <label className='input-label'>نوع الخدمة</label>
+    <div className="mb-3">
+      <div className="input-container">
+        <label className="input-label">نوع الخدمة</label>
         <select
-          name='service'
-          id='service'
-          className='input-field'
-          aria-placeholder='اختر نوع الخدمة'
+          name="service"
+          id="service"
+          className="input-field"
+          aria-placeholder="اختر نوع الخدمة"
         >
-          <option value='service1'>خدمة 1</option>
-          <option value='ُservice2'>خدمة 2</option>
+          <option value="service1">خدمة 1</option>
+          <option value="ُservice2">خدمة 2</option>
         </select>
       </div>
     </div>
-    <div className='mb-3'>
-      <div className='input-container'>
-        <label className='input-label'>تفاصيل عن الخدمة المطلوبة</label>
+    <div className="mb-3">
+      <div className="input-container">
+        <label className="input-label">تفاصيل عن الخدمة المطلوبة</label>
         <textarea
-          placeholder='اكتب تفاصيل عن الخدمة المطلوبة..'
-          className='input-field'
+          placeholder="اكتب تفاصيل عن الخدمة المطلوبة.."
+          className="input-field"
           rows={5}
         />
       </div>
     </div>
-    <button className='gradient-button'>
-      <span className='button-text'>ارسال طلب خدمة</span>
-      <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+    <button className="gradient-button">
+      <span className="button-text">ارسال طلب خدمة</span>
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
     </button>
   </form>
 );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,56 +20,56 @@ const Home = () => {
       <AboutUs></AboutUs>
       <Vision></Vision>
       {/* section 5 */}
-      <div id='why-we-are-here' className='we container-fluid mb-5'>
-        <div className='why-us-section py-5 text-white'>
-          <div className='container text-center'>
+      <div id="why-we-are-here" className="we container-fluid mb-5">
+        <div className="why-us-section py-5 text-white">
+          <div className="container text-center">
             {/* Section Title */}
-            <h4 className='mb-5 boldFont fontSize-32'>
-              لماذا <span className='secondary-color underLine'>نحن؟</span>
+            <h4 className="mb-5 boldFont fontSize-32">
+              لماذا <span className="secondary-color underLine">نحن؟</span>
             </h4>
 
             {/* Features Section */}
-            <div className='container boldFont'>
-              <div className='row'>
+            <div className="container boldFont">
+              <div className="row">
                 {/* Feature 1 */}
-                <div className='col-md-3 mb-4'>
-                  <div className='feature-item'>
-                    <div className='icon-circle text-white fontSize-40'>1</div>
-                    <h5 className='mt-3 fontSize-24'>مواصفات عالية</h5>
-                    <p className='regularFont fontSize-16'>
+                <div className="col-md-3 mb-4">
+                  <div className="feature-item">
+                    <div className="icon-circle text-white fontSize-40">1</div>
+                    <h5 className="mt-3 fontSize-24">مواصفات عالية</h5>
+                    <p className="regularFont fontSize-16">
                       مواصفات عالية بجودة ذات قيمة ومنظمة
                     </p>
                   </div>
                 </div>
 
                 {/* Feature 2 */}
-                <div className='col-md-3 mb-4'>
-                  <div className='feature-item'>
-                    <div className='icon-circle fontSize-40'>2</div>
-                    <h5 className='mt-3 fontSize-24'>نخبة من المهندسين</h5>
-                    <p className='regularFont fontSize-16'>
+                <div className="col-md-3 mb-4">
+                  <div className="feature-item">
+                    <div className="icon-circle fontSize-40">2</div>
+                    <h5 className="mt-3 fontSize-24">نخبة من المهندسين</h5>
+                    <p className="regularFont fontSize-16">
                       مجموعة من امهر المهندسين المحترفين
                     </p>
                   </div>
                 </div>
 
                 {/* Feature 3 */}
-                <div className='col-md-3 mb-4'>
-                  <div className='feature-item'>
-                    <div className='icon-circle fontSize-40'>3</div>
-                    <h5 className='mt-3 fontSize-24'>عقود قانونية</h5>
-                    <p className='regularFont fontSize-16'>
+                <div className="col-md-3 mb-4">
+                  <div className="feature-item">
+                    <div className="icon-circle fontSize-40">3</div>
+                    <h5 className="mt-3 fontSize-24">عقود قانونية</h5>
+                    <p className="regularFont fontSize-16">
                       عقود قانونية متوافقة مع قوانين الدولة
                     </p>
                   </div>
                 </div>
 
                 {/* Feature 4 */}
-                <div className='col-md-3 mb-4'>
-                  <div className='feature-item'>
-                    <div className='icon-circle fontSize-40'>4</div>
-                    <h5 className='mt-3 fontSize-24'>متابعة دائمة</h5>
-                    <p className='regularFont fontSize-16'>
+                <div className="col-md-3 mb-4">
+                  <div className="feature-item">
+                    <div className="icon-circle fontSize-40">4</div>
+                    <h5 className="mt-3 fontSize-24">متابعة دائمة</h5>
+                    <p className="regularFont fontSize-16">
                       متابعة دائمة وسهولة تواصل مع العملاء
                     </p>
                   </div>
@@ -81,24 +81,24 @@ const Home = () => {
       </div>
 
       {/* section 6 */}
-      <div id='services' className='services-section py-5'>
-        <div className='container text-center'>
+      <div id="services" className="services-section py-5">
+        <div className="container text-center">
           {/* Section Header */}
-          <h2 className='mb-5 section-header boldFont secondary-color fontSize-32'>
-            <span className='underLine'>الخدمات</span>{" "}
-            <span className='brandMain-color'> التي نقدمها</span>
+          <h2 className="mb-5 section-header boldFont secondary-color fontSize-32">
+            <span className="underLine">الخدمات</span>{" "}
+            <span className="brandMain-color"> التي نقدمها</span>
           </h2>
 
           {/* Services Row */}
-          <div className='row'>
+          <div className="row">
             {/* Card 1: مشاريع الطرق */}
-            <div className='col-md-4 mb-4'>
-              <div className='service-card p-4'>
-                <div className='icon-circle2 projectCard1 mb-3 service-card1 '>
-                  <img src={gameHome} alt='Roads' />
+            <div className="col-md-4 mb-4">
+              <div className="service-card p-4">
+                <div className="icon-circle2 projectCard1 mb-3 service-card1 ">
+                  <img src={gameHome} alt="Roads" />
                 </div>
-                <h5 className='mediumFont fontSize-24'>مشاريع الطرق</h5>
-                <p className='card-text regularFont fontSize-16'>
+                <h5 className="mediumFont fontSize-24">مشاريع الطرق</h5>
+                <p className="card-text regularFont fontSize-16">
                   تصميم، تنفيذ، وصيانة الطرق والبنية التحتية المرتبطة بها، بدءًا
                   من التخطيط الهندسي إلى تعبيد الأسطح وإصلاح الأضرار مع ضمان
                   الجودة والسلامة
@@ -106,13 +106,13 @@ const Home = () => {
               </div>
             </div>
             {/* Card 2: مشاريع المباني */}
-            <div className='col-md-4 mb-4'>
-              <div className='service-card p-4'>
-                <div className='icon-circle2 secondary-bgColor mb-3'>
-                  <img src={vector3Home} alt='Buildings' />
+            <div className="col-md-4 mb-4">
+              <div className="service-card p-4">
+                <div className="icon-circle2 secondary-bgColor mb-3">
+                  <img src={vector3Home} alt="Buildings" />
                 </div>
-                <h5 className='mediumFont fontSize-24'>مشاريع المباني</h5>
-                <p className='card-text regularFont fontSize-16'>
+                <h5 className="mediumFont fontSize-24">مشاريع المباني</h5>
+                <p className="card-text regularFont fontSize-16">
                   إدارة جميع مراحل البناء مثل التصميم، الإنشاء، التشطيبات،
                   والإشراف لضمان تنفيذ المشاريع بكفاءة ووفق الجداول الزمنية
                   المحددة.
@@ -120,15 +120,15 @@ const Home = () => {
               </div>
             </div>
             {/* Card 3: مشاريع البنية التحتية */}
-            <div className='col-md-4 mb-4'>
-              <div className='service-card p-4'>
-                <div className='icon-circle2 infra-bg mb-3'>
-                  <img src={vector4Home} alt='Infrastructure' />
+            <div className="col-md-4 mb-4">
+              <div className="service-card p-4">
+                <div className="icon-circle2 infra-bg mb-3">
+                  <img src={vector4Home} alt="Infrastructure" />
                 </div>
-                <h5 className='mediumFont fontSize-24'>
+                <h5 className="mediumFont fontSize-24">
                   مشاريع البنية التحتية
                 </h5>
-                <p className='card-text regularFont fontSize-16'>
+                <p className="card-text regularFont fontSize-16">
                   تصميم وتنفيذ وصيانة الطرق الأساسية مثل مشاريع الإنارة وشبكات
                   المياه والصرف الصحي، الطرق والجسور لضمان دعم المجتمعات
                   وتطويرها.
@@ -141,38 +141,38 @@ const Home = () => {
 
       {/* section 7 */}
 
-      <div id='reviews' className='bidSlider'>
-        <div className='review-background container-fluid bidSlider'>
-          <div className='boldFont fontSize-32 text-white d-flex text-decoration-underline justify-content-center align-items-center'>
-            <p className='mt-5'>الأراء</p>
+      <div id="reviews" className="bidSlider">
+        <div className="review-background container-fluid bidSlider">
+          <div className="boldFont fontSize-32 text-white d-flex text-decoration-underline justify-content-center align-items-center">
+            <p className="mt-5">الأراء</p>
           </div>
 
-          <div className='layered-container d-flex justify-content-center align-items-center'>
+          <div className="layered-container d-flex justify-content-center align-items-center">
             {/* Layer 1 */}
-            <div className='layer layer-1'></div>
+            <div className="layer layer-1"></div>
             {/* Layer 2 */}
-            <div className='layer layer-2'></div>
+            <div className="layer layer-2"></div>
             {/* Layer 3 */}
 
-            <div className='layer layer-3'>
+            <div className="layer layer-3">
               {/* Bootstrap Carousel */}
               <div
-                id='testimonialCarousel'
-                className='carousel slide'
-                data-bs-ride='carousel'
+                id="testimonialCarousel"
+                className="carousel slide"
+                data-bs-ride="carousel"
               >
-                <div className='dotted-box2'></div>
-                <div className='dotted-box3'></div>
-                <div className='carousel-inner'>
+                <div className="dotted-box2"></div>
+                <div className="dotted-box3"></div>
+                <div className="carousel-inner">
                   {/* First Slide */}
-                  <div className='carousel-item active'>
-                    <div className='testimonial-card mx-auto'>
-                      <img src={coloredVectorHome} alt='' />
-                      <h5 className='mediumFont fontSize-20 brandMain-color mt-3'>
+                  <div className="carousel-item active">
+                    <div className="testimonial-card mx-auto">
+                      <img src={coloredVectorHome} alt="" />
+                      <h5 className="mediumFont fontSize-20 brandMain-color mt-3 slider-margin">
                         Janna Khaled
                       </h5>
-                      <div className='stars mb-2 '>⭐⭐⭐⭐⭐</div>
-                      <p className='brandMain-color regularFont fontSize-20'>
+                      <div className="stars mb-2 ">⭐⭐⭐⭐⭐</div>
+                      <p className="brandMain-color regularFont fontSize-20">
                         شركة حدود البناء تتميز بالالتزام العالي بمواعيد التسليم
                         والجودة الممتازة في تنفيذ مشاريع البناء والبنية التحتية.
                         فريقها المتخصص يحرص على توفير حلول مبتكرة تلبي احتياجات
@@ -182,14 +182,14 @@ const Home = () => {
                     </div>
                   </div>
                   {/* Second Slide */}
-                  <div className='carousel-item active'>
-                    <div className='testimonial-card mx-auto'>
-                      <img src={coloredVectorHome} alt='' />
-                      <h5 className='mediumFont fontSize-20 brandMain-color mt-3'>
+                  <div className="carousel-item active">
+                    <div className="testimonial-card mx-auto">
+                      <img src={coloredVectorHome} alt="" />
+                      <h5 className="mediumFont fontSize-20 brandMain-color mt-3">
                         Janna Khaled
                       </h5>
-                      <div className='stars mb-2 '>⭐⭐⭐⭐⭐</div>
-                      <p className='brandMain-color regularFont fontSize-20'>
+                      <div className="stars mb-2 ">⭐⭐⭐⭐⭐</div>
+                      <p className="brandMain-color regularFont fontSize-20">
                         شركة حدود البناء تتميز بالالتزام العالي بمواعيد التسليم
                         والجودة الممتازة في تنفيذ مشاريع البناء والبنية التحتية.
                         فريقها المتخصص يحرص على توفير حلول مبتكرة تلبي احتياجات
@@ -199,31 +199,32 @@ const Home = () => {
                     </div>
                   </div>
                 </div>
-                <div className='container'>
+
+                <div className="container">
                   {/* Custom Navigation Arrows */}
                   <button
-                    className='carousel-control-prev custom-arrow'
-                    type='button'
-                    data-bs-target='#testimonialCarousel'
-                    data-bs-slide='prev'
+                    className="carousel-control-prev custom-arrow"
+                    type="button"
+                    data-bs-target="#testimonialCarousel"
+                    data-bs-slide="prev"
                   >
-                    <span className='icon-container3'>
+                    <span className="icon-container3">
                       <span
-                        className='carousel-control-prev-icon'
-                        aria-hidden='true'
+                        className="carousel-control-prev-icon"
+                        aria-hidden="true"
                       ></span>
                     </span>
                   </button>
                   <button
-                    className='carousel-control-next custom-arrow'
-                    type='button'
-                    data-bs-target='#testimonialCarousel'
-                    data-bs-slide='next'
+                    className="carousel-control-next custom-arrow"
+                    type="button"
+                    data-bs-target="#testimonialCarousel"
+                    data-bs-slide="next"
                   >
-                    <span className='icon-container3'>
+                    <span className="icon-container3">
                       <span
-                        className='carousel-control-next-icon'
-                        aria-hidden='true'
+                        className="carousel-control-next-icon"
+                        aria-hidden="true"
                       ></span>
                     </span>
                   </button>
